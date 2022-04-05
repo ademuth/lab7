c37f0e7310f139ee8b7ea21fa09525500e7211ef
ademuth <ademuth1@hawk.iit.edu>
2022-04-05 15:33:37 -0500

    Fixed capitalization and quotation mistakes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ function init(){
 //add your javascript between these two lines of code
   function alertFunction(){
 	var x1 = document.getElementById("entryinput").value;
-	var x2 = document.getelementById("textoutput");
+	var x2 = document.getElementById("textoutput");
 	alert("Alex DeMuth: "+ x1);
 	x1.innerHTML = x1;
   }
-  var x3 = document.getElementById("entrybutton");
+  var x3 = document.getElementById('entrybutton');
   x3.addEventListener("click", alertFunction);
 }
 window.addEventListener('load', init);
