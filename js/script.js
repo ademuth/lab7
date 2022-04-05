44202ab03a68f54a184b00c283111105a8df4119
ademuth <ademuth1@hawk.iit.edu>
2022-04-05 15:06:01 -0500

    renamed lab7.html to index.html and added alert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+  function alertFunction(){
+	var x1 = document.getElementById("entryinput").value;
+	var x2 = document.getelementById("textoutput");
+	
+	alert("Alex DeMuth: "+ x1);
+	x1.innerHTML = x1;
+  }
 
 
 
